d488de2b922b4b8fbed98473a4e185508f5b9669
feinerheiner <58785706+feinerheiner@users.noreply.github.com>
2020-07-29 19:05:15 -0600

    Game.java is the main class, RockPaperScissors is the suport class

---
+++ RockPaperScissors.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+class RockPaperScissors
+{
+   public String RockPaperScissors  ()
+   {
+      Scanner in = new Scanner(System.in);
+      Random r = new Random();
+      
+      int cpu = 0;
+      String user = "";
+      
+      //Users choice
+      try{
+         System.out.println("Please choose \"Rock\", \"Paper\". or \"Scissors\".");
+         user = in.nextLine();
+      }
+      catch (InputMissmatchException){
+         System.out.println("
+      }
+      //Cpus choice
+      
+      
+   }
+}
